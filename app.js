f5241acbb155ee06ba93a6668b4ce015bd66d3e0
mickeysrishti <Mukeshpathak345@gmail.com>
2024-05-22 02:09:37 +0530

    Day 3 Code added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express(); 
-
-const cookieParser = require("cookies-parser");
+const cookieParser = require("cookie-parser"); 
 const path = require("path");
+
+const db = require('./config/mongoose-connection');
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +14,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', "ejs");
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
 
 app.listen(3000);
